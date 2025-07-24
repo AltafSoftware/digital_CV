74e89e77eb7e7664e983b927d984776796b0e157
Altaf H. Rezai <202209871@post.au.dk>
2025-07-24 14:44:45 +0200

    Enhance About and Navbar components with tools display and scroll effect

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
 
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
 
   const openMenu = () => {
@@ -14,19 +15,27 @@ const Navbar = () => {
     sideMenuRef.current.style.transform = 'translateX(16rem)';
   }
 
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    })
+  }, [])
+
   return (
     <>
       <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt="" className="w-full" />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 
-      flex items-center justify-between z-50 ">
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 transition-all duration-300 ${isScroll ? "bg-white/20 backdrop-blur-md shadow-lg" : ""}`}>
         <a href="#top">
           <Image src={assets.logo} alt="" className="w-28 cursor-pointer mr-14" />
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8
-        rounded-full px-12 py-3 bg-white shadow-sml bg-opcaity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 transition-all duration-300 ${isScroll ? "" : "bg-white/50 shadow-sm backdrop-blur-sm"}`}>
           <li><a href="#top">Home</a></li>
           <li><a href="#about">About Me</a></li>
           <li><a href="#services">Services</a></li>
